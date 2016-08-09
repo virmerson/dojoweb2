36c8b0782509460c67bc835044d35f109c34aa69
virmerson <virmerson@hightechcursos.com.br>
2016-08-09 19:55:03 -0400

    Criando Projeto Base

---
+++ src/main/java/br/com/fabricadeprogramador/ControlepresencaApplication.java
@@ -0,0 +1,12 @@
+package br.com.fabricadeprogramador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlepresencaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlepresencaApplication.class, args);
+	}
+}
